41ae2fe7152a0d757d849fa9c656d040efebf32f
Elie Schoppik <eschoppik@gmail.com>
2016-10-06 15:02:49 -0700

    fixing doubleLetterCount

--- functions_exercise/solutions.js
+++ functions_exercise/solutions.js
@@ -46,10 +46,10 @@ function singleLetterCount(str1, letter){
 function multipleLetterCount(str){
   var finalObj = {};
   for(var i =0; i< str.length; i++){
-    if (str[i] in finalObj){
-      finalObj[str[i]]++;
-    } else {
+    if (!(str[i] in finalObj)){
       finalObj[str[i]] = 1;
+    } else {
+      finalObj[str[i]]++;
     }
   }
   return finalObj;
